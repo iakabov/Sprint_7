8c2dc7e9ba65bdbc5a22a207cf59ede43351f2e9
1ldar <iakabov1621@gmail.com>
2026-05-29 20:07:42 +0300

    Спринт7 v1.0

--- src/main/java/ru/yandex/practicum/client/Client.java
+++ src/main/java/ru/yandex/practicum/client/Client.java
@@ -6,7 +6,7 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
 
 public class Client {
-    public static final String BASE_URI = "http://qa-scooter.praktikum-services.ru";
+    public static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
     public static final String BASE_PATH = "/api/v1";
 
     public RequestSpecification spec() {
--- src/test/java/ru/yandex/practicum/LoginTest.java
+++ src/test/java/ru/yandex/practicum/LoginTest.java
@@ -58,8 +58,8 @@ public class LoginTest extends BaseTest {
 
         Response response = courierClient.login(courierForAuth);
 
-        response.then().statusCode(HttpStatus.SC_GATEWAY_TIMEOUT);
-//                .and().body(FIELD_MESSAGE, equalTo(MESSAGE_NO_ENOUGH_DATA)); должна быть ошибка 400, но возвращается 509, даже через постман
+        response.then().statusCode(HttpStatus.SC_GATEWAY_TIMEOUT)
+                .and().body(FIELD_MESSAGE, equalTo(MESSAGE_NO_ENOUGH_DATA));
     }
 
     @Test
@@ -80,8 +80,8 @@ public class LoginTest extends BaseTest {
 
         Response response = courierClient.login(courierForAuthWithPasswordNull);
 
-        response.then().statusCode(HttpStatus.SC_GATEWAY_TIMEOUT);
-//                .and().body(FIELD_MESSAGE, equalTo(MESSAGE_NO_ENOUGH_DATA)); должна быть ошибка 400, но возвращается 509, даже через постман
+        response.then().statusCode(HttpStatus.SC_GATEWAY_TIMEOUT)
+                .and().body(FIELD_MESSAGE, equalTo(MESSAGE_NO_ENOUGH_DATA));
     }
 
     @Test
